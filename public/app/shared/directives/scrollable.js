9e28ae346e2a91ab4a3c088116adc487e5d2ab8c
lunxhboxsuhi <morozoandrei@gmail.com>
2015-10-01 11:06:32 -0400

    added params

--- public/app/shared/directives/scrollable.js
+++ public/app/shared/directives/scrollable.js
@@ -1,4 +1,4 @@
-angular.module('app').directive('scrollable',['$window', function() {
+angular.module('app').directive('scrollable',['$window', function($window) {
     return function(scope, elem, attrs) {
         elem.height = $window.innerHeight;
     }
