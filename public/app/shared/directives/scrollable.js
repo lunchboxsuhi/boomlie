7b9770fecc44b7fce9e502d7f86bab349d6301eb
lunxhboxsuhi <morozoandrei@gmail.com>
2015-10-01 12:28:57 -0400

    c

--- public/app/shared/directives/scrollable.js
+++ public/app/shared/directives/scrollable.js
@@ -1,6 +1,10 @@
 angular.module('app').directive('scrollable',['$window', function($window) {
-    return function(scope, elem, attrs) {
-        elem.height = "400px"
-        console.log(elem.height);
+    return function (scope, element) {
+        var w = angular.element($window);
+        var changeHeight = function() {element.css('height', ($window.innerHeight - 100) + 'px' );};
+        w.bind('resize', function () {
+            changeHeight();   // when window size gets changed
+        });
+        changeHeight(); // when page loads
     }
 }]);
